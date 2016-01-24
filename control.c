54d671cd8fd8c20caa99614b848ceb0cdbaf1853
Marina Brown <catskillmarina@gmail.com>
2016-01-24 11:16:35 +0000

    functions finally working properly

--- control.c
+++ control.c
@@ -21,21 +21,14 @@ void open_configure()
 {
     int fcarg;
 
-    printf("In open_configure !\n");
     serial_filedesc = open("/dev/ttyU0", O_RDWR | O_NOCTTY | O_NDELAY);
     if(!serial_filedesc)
     {
         perror("could not open /dev/ttyU0 -");
     }
-    else
-    {
-        printf("Serial port /dev/ttyU0 open filedesc %d \n.", n);
-    }
     tcgetattr(serial_filedesc,&options);
     speed = cfsetispeed(&options,B115200);
-    printf("speed set to %d.\n",speed);
     speed = cfsetospeed(&options,B115200);
-    printf("speed set to %d.\n",speed);
     options.c_cflag |= (CLOCAL | CREAD );
 
     /* 8N1 */
@@ -60,7 +53,6 @@ void open_configure()
 
     /* set the options */
     tcsetattr(serial_filedesc, TCSANOW, &options);
-    printf("Leaving open_configure !\n");
 }
 
 
@@ -75,18 +67,28 @@ int close_device ()
 }
 
 /******************************************************
+*                  let_relay_rest                     *
+******************************************************/
+void let_relay_rest(void)
+{
+    int mcs = 100000;
+
+    usleep(mcs);
+    usleep(mcs); 
+    usleep(mcs);
+}
+
+/******************************************************
 *                  re_open_device                     *
 *******************************************************/
 void re_open_device ()
 {
     int j = 0;
 
-    printf("in -- re_open_device -- Letting it rest for a few seconds i = %d !\n", i);
     {
         tcflow(serial_filedesc,TCOON);
         close_device();
-        for (j=0;j<=12;j++)
-            usleep(700000);
+        usleep(400000);
         open_configure();
     }
 }
@@ -95,15 +97,11 @@ void re_open_device ()
 *                  get_model_name                     *
 *******************************************************/
 
-get_model_name()
+char *get_model_name()
 {
-    int j;
-    printf("In get Model Name !\n");
-    bzero(input_buffer, 255);
+    int j, i = 0, n = 0, bytes_waiting = 0;
 
-    bytes_waiting = 0;
-    n = 0;
-    i = 0;
+    bzero(input_buffer, 255);
     while(bytes_waiting <= 0)
     {
         i++;
@@ -111,31 +109,25 @@ get_model_name()
         tcdrain(serial_filedesc);
         ioctl(serial_filedesc, FIONREAD, &bytes_waiting);
         n = read(serial_filedesc,input_buffer,bytes_waiting); 
-        printf("GMN There are %d bytes waiting to be read. -> %s\n", bytes_waiting, input_buffer);
-        usleep(500000);
-
-        if(i >= 20)
+        if(i >= 5)
         {
             re_open_device();
             i = 0;
         }
              
     }
-    printf("This is how many chars written %d.\n", n);
-    n = read(serial_filedesc,input_buffer,bytes_waiting); 
-    printf("%s\n",input_buffer);
-    printf("Leaving get Model Name !\n");
+
+    return input_buffer;
 }
 
 /******************************************************
 *                  get_version_number                 *
 *******************************************************/
-get_version_number()
+char *get_version_number()
 {
     int i =0;
     int j = 0;
 
-    printf("In Get version Number !\n");
     bzero(input_buffer, 255);
     bytes_waiting = 0;
     n = 0;
@@ -145,18 +137,15 @@ get_version_number()
         tcdrain(serial_filedesc);
         ioctl(serial_filedesc, FIONREAD, &bytes_waiting);
         n = read(serial_filedesc,input_buffer,bytes_waiting); 
-        printf("GET VER There are %d bytes waiting to be read. -> %s \n", bytes_waiting, input_buffer);
-        usleep(500000);
-        if(i >= 20)
+        if(i >= 5)
         {
             re_open_device(); 
             i = 0;
         }
+        i++;
     }
-    printf("This is how many chars written %d. \n", n);
-    n = read(serial_filedesc,input_buffer,bytes_waiting); 
-    printf("%s\n",input_buffer);
-    printf("In Get version Number !\n");
+
+    return input_buffer;
 }
 
 /******************************************************
@@ -166,7 +155,6 @@ int get_relay_status(int relay)
 {
     int i = 0, j=0;
 
-    printf("In get_relay_status %d!\n", relay);
     bzero(input_buffer, 255);
     bytes_waiting = 0;
     n = 0;
@@ -177,23 +165,16 @@ int get_relay_status(int relay)
     {
         i++;
         n = write(serial_filedesc,foo,3);
+        tcdrain(serial_filedesc);
         ioctl(serial_filedesc, FIONREAD, &bytes_waiting);
         n = read(serial_filedesc,input_buffer,bytes_waiting); 
-        tcdrain(serial_filedesc);
-        usleep(500000); 
-        printf("GET RELAY STATUS There are %d bytes waiting to be read. -> %s \n", bytes_waiting, input_buffer);
-        if(i >= 20)
+        if(i >= 5)
         {
             re_open_device();
             i = 0;
         }
 
     }
-    printf("This is how many chars written %d. \n", n);
-    n = read(serial_filedesc,input_buffer,bytes_waiting); 
-    tcdrain(serial_filedesc);
-    printf("Relay Status ---> %s \n", input_buffer);
-    printf("Leaving get_relay_status !\n");
 
     return atoi(input_buffer);
 }
@@ -201,7 +182,7 @@ int get_relay_status(int relay)
 /******************************************************
 *                  switch_relay                       *
 *******************************************************/
-int switch_relay(int relay,int action)
+char *switch_relay(int relay,int action)
 {
     int bytes_waiting = 0;
     int n = 0;
@@ -209,12 +190,9 @@ int switch_relay(int relay,int action)
     int j = 0;
     char foo[256];
 
-    printf("/*************************************************/\n");
-    printf("In switch_relayRelay -> %d! Action -> \n", relay, action);
     bzero(input_buffer, 255);
     bzero(foo,255);
     sprintf(foo, "S%d=%d\n", relay,action);
-    printf("%s\n",foo);
 
     while(bytes_waiting <= 0)
     {
@@ -223,20 +201,15 @@ int switch_relay(int relay,int action)
         tcdrain(serial_filedesc);
         ioctl(serial_filedesc, FIONREAD, &bytes_waiting);
         n = read(serial_filedesc,input_buffer,bytes_waiting); 
-        usleep(500000);
-        printf("switch_relay There are %d bytes waiting 2 be read. -> %s \n", bytes_waiting, input_buffer);
-        if(i >= 20)
+        if(i >= 5)
         {
             re_open_device();
             i = 0;
         }
 
     }
-    printf("This is how many chars written %d. \n", n);
-    n = read(serial_filedesc,input_buffer,bytes_waiting); 
-    printf("switch_relay ---> %s \n", input_buffer);
-    printf("Leaving switch_relay !\n");
-    printf("/*************************************************/\n");
+
+    return input_buffer;
 }
 
 /*******************************************************
@@ -246,35 +219,29 @@ main ()
 {
 
     open_configure();
-/*    get_model_name();
-    usleep(500000);usleep(500000);usleep(500000);
-    get_version_number();
-*/
-
+    printf("This relay has model name -> %s",get_model_name());
+    printf("The version number is -> %s",get_version_number());
     printf("Relay status 1 is -> %d.\n", get_relay_status(1));
     printf("Relay status 2 is -> %d.\n", get_relay_status(2));
     printf("Relay status 3 is -> %d.\n", get_relay_status(3));
     printf("Relay status 4 is -> %d.\n", get_relay_status(4));
-
-/*
-    usleep(500000);usleep(500000);usleep(500000); 
-    switch_relay(1,1);
-    usleep(500000);usleep(500000);usleep(500000);
-    switch_relay(2,1);
-    usleep(500000);usleep(500000);usleep(500000);
-    switch_relay(3,1);
-    usleep(500000);usleep(500000);usleep(500000);
-    switch_relay(4,1);
-    usleep(500000);usleep(500000);usleep(500000);
-    switch_relay(1,0);
-    usleep(500000);usleep(500000);usleep(500000);
-    switch_relay(2,0);
-    usleep(500000);usleep(500000);usleep(500000);
-    switch_relay(3,0);
-    usleep(500000);usleep(500000);usleep(500000);
-    switch_relay(4,0);
-    usleep(500000);usleep(500000);usleep(500000);
-*/
+    printf("%s",switch_relay(1,1));
+    printf("%s",switch_relay(2,1));
+    printf("%s",switch_relay(3,1));
+    printf("%s",switch_relay(4,1));
+    printf("Relay status 1 is -> %d.\n", get_relay_status(1));
+    printf("Relay status 2 is -> %d.\n", get_relay_status(2));
+    printf("Relay status 3 is -> %d.\n", get_relay_status(3));
+    printf("Relay status 4 is -> %d.\n", get_relay_status(4));
+    printf("%s",switch_relay(1,0));
+    printf("%s",switch_relay(2,0));
+    printf("%s",switch_relay(3,0));
+    printf("%s",switch_relay(4,0)); 
+    printf("Relay status 1 is -> %d.\n", get_relay_status(1));
+    printf("Relay status 2 is -> %d.\n", get_relay_status(2));
+    printf("Relay status 3 is -> %d.\n", get_relay_status(3));
+    printf("Relay status 4 is -> %d.\n", get_relay_status(4));
+    close_device();
 
     /* fcntl(serial_filedesc, F_SETFL, 0); */
     
